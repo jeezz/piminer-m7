14880b044be117b2892c9eff201895cb95f5087c
jeezz <info@hauptstadtspaziergang.de>
2017-12-06 01:31:05 +0100

    lets have it a try

--- piminer-install.sh
+++ piminer-install.sh
@@ -19,18 +19,18 @@ sed -i -e 's/-march=native/-mcpu=cortex-a53/g' m7/Makefile
 make -j4 > /dev/null
 
 ## get RPi-gov
-echo "Cloning rpi-cpu.gov
+echo "Cloning rpi-cpu.gov"
 wget https://raw.githubusercontent.com/DavidM42/rpi-cpu.gov/master/install.sh > /dev/null
 sudo chmod +x install.sh
 sudo ./install.sh --nochown
 sudo rm install.sh
 
 ##get piminer-start
-echo "Downloading piminer-start.sh
+echo "Downloading piminer-start.sh"
 wget https://raw.githubusercontent.com/jeezz/piminer/master/piminer-start.sh > /dev/null
 chmod +x piminer-start.sh
 
 ## change CPU-Governor
 read -n 1 -s -r -p "You can coose to change the CPU Governor. Please be carefull if you don't use heatspreader"
 cpu.gov
-echo Check the wolf-m7m-cpuminer folder and start the miner with ./piminer-start.sh
+echo Check the /pminer/wolf-m7m-cpuminer folder and start the miner with ./piminer-start.sh
