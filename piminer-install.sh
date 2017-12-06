f5964230d63cd7817f07a83aa5758f1b0b6fab4a
jeezz <info@hauptstadtspaziergang.de>
2017-12-06 01:22:19 +0100

    Update piminer-install.sh

--- piminer-install.sh
+++ piminer-install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ##Installing Essentials
-echo " Installing Essentials ..."
+echo "Installing Essentials ..."
 sudo apt-get update -y > /dev/null
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev > /dev/null
 
 ## Cloning Wolf-m7m-cpuminer
 mkdir piminer
 cd piminer
-git clone https://github.com/novaspirit/wolf-m7m-cpuminer
+git clone https://github.com/novaspirit/wolf-m7m-cpuminer > /dev/null
 
 ## building Wold-m7m-miner
+echo " Building wolf-m7m-miner"
 cd wolf-m7m-cpuminer
 ./autogen.sh
 ./configure CFLAGS="-mfpu=neon-vfpv4" > /dev/null
@@ -18,13 +19,15 @@ sed -i -e 's/-march=native/-mcpu=cortex-a53/g' m7/Makefile
 make -j4 > /dev/null
 
 ## get RPi-gov
-wget https://raw.githubusercontent.com/DavidM42/rpi-cpu.gov/master/install.sh
+echo "Cloning rpi-cpu.gov
+wget https://raw.githubusercontent.com/DavidM42/rpi-cpu.gov/master/install.sh > /dev/null
 sudo chmod +x install.sh
 sudo ./install.sh --nochown
 sudo rm install.sh
 
 ##get piminer-start
-wget https://raw.githubusercontent.com/jeezz/piminer/master/piminer-start.sh
+echo "Downloading piminer-start.sh
+wget https://raw.githubusercontent.com/jeezz/piminer/master/piminer-start.sh > /dev/null
 chmod +x piminer-start.sh
 
 ## change CPU-Governor
